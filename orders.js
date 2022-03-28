31fedf35c26f89a1cf401406d0f1614f9e8ae125
kluivert-ssali <kluivertssali@gmail.com>
2022-03-28 13:22:23 +0300

    first commit

---
+++ orders.js
@@ -0,0 +1,43 @@
+const Orders =[
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85321',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Skywalker SK21 Drone',
+        productNumber: '86321',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+
+    {
+        productName: 'HoverPad Drone',
+        productNumber: '89542',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Xiaomi Mi-Ni',
+        productNumber: '75321',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'DJI Mini Drone',
+        productNumber: '75321',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Rukoku puppet Drone',
+        productNumber: '98321',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
